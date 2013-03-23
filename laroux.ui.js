cae5b74e90ce20acc635ac8a618f2579160b52b8
larukedi <eser@sent.com>
2013-03-23 10:26:38 +0200

    * removed jquery/jqueryui routines and dependencies. * updated id/css naming * css transitions

--- laroux.ui.js
+++ laroux.ui.js
@@ -7,7 +7,7 @@
 
         createFloatContainer: function() {
             if (!laroux.ui.floatContainer) {
-                laroux.ui.floatContainer = laroux.dom.createElement('DIV', { id: 'floatDiv' }, '');
+                laroux.ui.floatContainer = laroux.dom.createElement('DIV', { id: 'laroux_floatdiv' }, '');
                 document.body.insertBefore(laroux.ui.floatContainer, document.body.firstChild);
             }
         },
@@ -20,77 +20,14 @@
 
         msgbox: function(timeout, message) {
             var id = laroux.helpers.getUniqueId();
-            laroux.ui.floatContainer.appendChild(laroux.ui.createBox(id, 'msgbox', message));
-            var obj = $('#' + id);
-            $(obj).fadeIn('slow');
-            laroux.timers.set(timeout, function(x) { $(x).fadeOut('slow'); }, obj);
-        },
-        
-        autocomplete: function(obj, settings) {
-            if (typeof $.ui.autocomplete !== 'undefined') {
-                $.widget('custom.catcomplete', $.ui.autocomplete, {
-                    _renderMenu: function(ul, items) {
-                        var self = this,
-                        currentCategory = '';
-                        
-                        $.each(items, function(index, item) {
-                            if (typeof self.options.categoryfieldname !== 'undefined' && item[self.options.categoryfieldname] != currentCategory) {
-                                ul.append('<li class="ui-autocomplete-category">' + item[self.options.categoryfieldname] + '</li>');
-                                currentCategory = item[self.options.categoryfieldname];
-                            }
-                            
-                            var text = '<a><span class="label">' + item[self.options.labelfieldname] + '</span>';
-                            if (typeof self.options.descriptionfieldname !== 'undefined') {
-                                text += '<span class="desc">' + item[self.options.descriptionfieldname] + '</span></a>';
-                            }
+            laroux.ui.floatContainer.appendChild(laroux.ui.createBox(id, 'laroux_msgbox', message));
 
-                            ul.append($('<li></li>').data('item.autocomplete', item).append(text));
-                            // self._renderItem(ul, item);
-                        });
-                    },
-                    dropdown: function() {
-                        this.element.focus();
-                        this.search('', null);
-                    }
-                });
-            }
-            
-            settings.select = function(event, ui) {
-                if (typeof settings.hiddenfield !== 'undefined') {
-                    settings.hiddenfield.val(ui.item[settings.hiddenfieldname]);
-                }
-                
-                if (typeof settings.onselect !== 'undefined') {
-                    settings.onselect(event, ui);
-                }
-            };
-            
-            obj.click(function() {
-                var catcomplete = obj.data('catcomplete');
-                catcomplete.search((catcomplete.options.alllist === true) ? '' : null, null);
-            });
+            var obj = laroux.dom.select('#' + id);
+            laroux.css.setProperty(obj, 'opacity', '1');
 
-            obj.addClass('catcomplete');
-            obj.catcomplete(settings);
-
-            if (typeof settings.hiddenfield !== 'undefined') {
-                for (x in settings.source) {
-                    if (settings.source[x][settings.hiddenfieldname] == settings.hiddenfield.val()) {
-                        obj.val(settings.source[x].label);
-                        break;
-                    }
-                }
-            }
-        },
-        
-        datepicker: function(obj, settings) {
-            settings.changeMonth = true;
-            settings.changeYear = true;
-            if (typeof settings.dateFormat == 'undefined') {
-                settings.dateFormat = 'dd/mm/yy';
-            }
-            
-            obj.datepicker(settings);
+            laroux.timers.set(timeout, function(x) {
+                laroux.css.setProperty(x, 'opacity', '0');
+            }, obj);
         }
     };
 
