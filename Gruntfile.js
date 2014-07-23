db3030c3f3a2ad047cfcac30796d07c7fc622803
larukedi <eser@sent.com>
2014-07-23 08:17:14 +0300

    * grunt tasks are reorganized.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,23 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            css: {
+                options: {
+                    compress: false,
+                    yuicompress: false,
+                    optimization: 0
+                },
+                files: {
+                    'temp/laroux.ui.css': [ 'src/laroux.ui.less' ]
+                }
+            }
+        },
         concat: {
-            options: {
-                separator: ';'
-            },
-            dist: {
+            js: {
+                options: {
+                    separator: ';'
+                },
                 src: [
                     'src/laroux.js',
                     'src/laroux.ajax.js',
@@ -25,20 +37,31 @@ module.exports = function(grunt) {
                     'src/laroux.ui.js'
                 ],
                 dest: 'dist/<%= pkg.name %>.js'
+            },
+            css: {
+                src: [
+                    'temp/laroux.ui.css'
+                ],
+                dest: 'dist/<%= pkg.name %>.css'
             }
         },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
             },
-            dist: {
+            js: {
                 files: {
-                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
                 }
             }
         },
+        cssmin: {
+            css: {
+                src: 'dist/<%= pkg.name %>.css',
+                dest: 'dist/<%= pkg.name %>.min.css'
+            }
+        },
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js'],
             options: {
                 // options here to override JSHint defaults
                 globals: {
@@ -47,38 +70,54 @@ module.exports = function(grunt) {
                     module: true,
                     document: true
                 }
-            }
+            },
+            files: ['Gruntfile.js', 'src/**/*.js']
         },
         watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['jshint']
-        },
-        copy: {
-            main: {
-                files: [
-                    { expand: true, cwd: 'src/', src: ['**/*.css'], dest: 'dist/', filter: 'isFile' }
-                ]
+            less: {
+                files: ['src/**/*.less'],
+                tasks: ['less:css'],
+                options: {
+                    nospawn: true
+                }
+            },
+            css: {
+                files: ['<%= concat.css.src %>'],
+                tasks: ['concat:css', 'cssmin:css']
+            },
+            js: {
+                files: ['<%= concat.js.src %>'],
+                tasks: ['jshint', 'concat:js', 'uglify:js']
             }
         },
         clean: {
-            dist: {
+            all: {
                 src: [
                     'dist/<%= pkg.name %>.js',
                     'dist/<%= pkg.name %>.min.js',
-                    'dist/*.css'
+                    'dist/<%= pkg.name %>.css',
+                    'dist/<%= pkg.name %>.min.css'
+                ]
+            },
+            temp: {
+                src: [
+                    '<%= concat.css.src %>'
                 ]
             }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('test', ['jshint']);
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'copy']);
+    grunt.registerTask('js', ['jshint', 'concat:js', 'uglify:js']);
+    grunt.registerTask('css', ['less:css', 'concat:css', 'cssmin:css']);
+    grunt.registerTask('default', ['jshint', 'concat:js', 'uglify:js', 'less:css', 'concat:css', 'cssmin:css', 'clean:temp']); // , 'copy'
 
 };
