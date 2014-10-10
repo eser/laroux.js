e0c1d27c1254d20f029d3ac7df82b974e5f9b0cd
Eser Ozvataf <eser.ozvataf@zaimogluholding.com>
2014-10-10 11:56:25 +0300

    * added $l.ui.loading.

--- src/laroux.ui.js
+++ src/laroux.ui.js
@@ -4,7 +4,85 @@
     // ui
     laroux.ui = {
         floatContainer: null,
-        defaultPopupTimeout: 5,
+
+        popup: {
+            defaultTimeout: 5,
+
+            createBox: function(id, xclass, message) {
+                return laroux.dom.createElement('DIV', { 'id': id, 'class': xclass },
+                    message
+                );
+            },
+
+            msgbox: function(timeout, message) {
+                var id = laroux.helpers.getUniqueId();
+                var obj = laroux.ui.popup.createBox(id, 'laroux_msgbox', message);
+                laroux.ui.floatContainer.appendChild(obj);
+
+                laroux.css.setProperty(obj, 'opacity', '1');
+
+                laroux.timers.set(timeout, function(x) {
+                    // laroux.css.setProperty(x, 'opacity', '0');
+                    laroux.dom.remove(x);
+                }, obj);
+            },
+
+            init: function() {
+                laroux.popupFunc = function(message) {
+                    laroux.ui.popup.msgbox(laroux.ui.popup.defaultTimeout, message);
+                };
+            }
+        },
+
+        loading: {
+            elementSelector: null,
+            element: null,
+            defaultDelay: 1500,
+            timer: null,
+
+            killTimer: function() {
+                window.clearTimeout(laroux.ui.loading.timer);
+            },
+
+            hide: function() {
+                laroux.ui.loading.killTimer();
+
+                laroux.css.setProperty(laroux.ui.loading.element, 'display', 'none');
+                localStorage.loadingIndicator = 'false';
+            },
+
+            show: function(delay) {
+                laroux.ui.loading.killTimer();
+
+                if (typeof delay == 'undefined') {
+                    delay = laroux.ui.loading.defaultDelay;
+                }
+
+                if (delay > 0) {
+                    window.setTimeout(function() { laroux.ui.loading.show(0); }, delay);
+                } else {
+                    laroux.css.setProperty(laroux.ui.loading.element, 'display', 'block');
+                    localStorage.loadingIndicator = 'true';
+                }
+            },
+
+            init: function() {
+                if (laroux.ui.loading.element === null && laroux.ui.loading.elementSelector !== null) {
+                    laroux.ui.loading.element = laroux.dom.selectSingle(laroux.ui.loading.elementSelector);
+                }
+
+                if (laroux.ui.loading.element !== null) {
+                    laroux.dom.setEvent(window, 'load', laroux.ui.loading.hide);
+                    laroux.dom.setEvent(window, 'beforeunload', laroux.ui.loading.show);
+
+                    if (typeof localStorage.loadingIndicator != 'undefined' && localStorage.loadingIndicator == 'true') {
+                        laroux.ui.loading.show(0);
+                    } else {
+                        laroux.ui.loading.show();
+                    }
+                }
+            }
+        },
 
         createFloatContainer: function() {
             if (!laroux.ui.floatContainer) {
@@ -13,30 +91,10 @@
             }
         },
 
-        createBox: function(id, xclass, message) {
-            return laroux.dom.createElement('DIV', { 'id': id, 'class': xclass },
-                message
-            );
-        },
-
-        msgbox: function(timeout, message) {
-            var id = laroux.helpers.getUniqueId();
-            var obj = laroux.ui.createBox(id, 'laroux_msgbox', message);
-            laroux.ui.floatContainer.appendChild(obj);
-
-            laroux.css.setProperty(obj, 'opacity', '1');
-
-            laroux.timers.set(timeout, function(x) {
-                // laroux.css.setProperty(x, 'opacity', '0');
-                laroux.dom.remove(x);
-            }, obj);
+        init: function() {
+            laroux.ui.createFloatContainer();
+            laroux.ui.popup.init();
+            laroux.ui.loading.init();
         }
     };
-
-    laroux.ready(laroux.ui.createFloatContainer);
-
-    laroux.popupFunc = function(message) {
-        laroux.ui.msgbox(laroux.ui.defaultPopupTimeout, message);
-    };
-
 })(this.laroux);
