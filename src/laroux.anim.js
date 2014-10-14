18232931db163e8479a74de3345a3377ad8c93c5
larukedi <eser@sent.com>
2014-10-14 15:52:28 +0300

    * fixed anim bug.

--- src/laroux.anim.js
+++ src/laroux.anim.js
@@ -7,7 +7,6 @@
         set: function(newanim) {
             if (typeof newanim.from != 'undefined' && newanim.from !== null) {
                 newanim.object[newanim.property] = newanim.from;
-                // newanim.target = newanim.from;
             }
 
             // if (typeof newanim.id == 'undefined') {
@@ -24,9 +23,7 @@
 
         ontick: function(newanim) {
             var current = newanim.object[newanim.property];
-            // var current = newanim.target;
             var diff = newanim.to - current;
-            console.log(diff);
 
             if (diff === 0) {
                 return false;
@@ -36,10 +33,14 @@
 
             if (diff > 0) {
                 newanim.object[newanim.property] += step;
-                // newanim.target += step;
+                if (newanim.object[newanim.property] > newanim.to) {
+                    newanim.object[newanim.property] = newanim.to;
+                }
             } else {
                 newanim.object[newanim.property] -= step;
-                // newanim.target -= step;
+                if (newanim.object[newanim.property] < newanim.to) {
+                    newanim.object[newanim.property] = newanim.to;
+                }
             }
         }
     };
