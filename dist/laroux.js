ee19d59fa1fea06f82d599a8891bf1be9287fad3
larukedi <eser@sent.com>
2014-10-18 02:33:43 +0300

    * dist.

--- dist/laroux.js
+++ dist/laroux.js
@@ -297,54 +297,6 @@
 ;(function(laroux) {
     "use strict";
 
-    // anim
-    laroux.anim = {
-        // { object, property, from, to, step }
-        set: function(newanim) {
-            if (typeof newanim.from != 'undefined' && newanim.from !== null) {
-                newanim.object[newanim.property] = newanim.from;
-            }
-
-            // if (typeof newanim.id == 'undefined') {
-            //     newanim.id = laroux.helpers.getUniqueId();
-            // }
-
-            laroux.timers.set({
-                timeout: 1,
-                reset: true,
-                ontick: laroux.anim.ontick,
-                state: newanim
-            });
-        },
-
-        ontick: function(newanim) {
-            var current = newanim.object[newanim.property];
-            var diff = newanim.to - current;
-
-            if (diff === 0) {
-                return false;
-            }
-
-            var step = (typeof newanim.step != 'undefined') ? newanim.step : 1;
-
-            if (diff > 0) {
-                newanim.object[newanim.property] += step;
-                if (newanim.object[newanim.property] > newanim.to) {
-                    newanim.object[newanim.property] = newanim.to;
-                }
-            } else {
-                newanim.object[newanim.property] -= step;
-                if (newanim.object[newanim.property] < newanim.to) {
-                    newanim.object[newanim.property] = newanim.to;
-                }
-            }
-        }
-    };
-
-})(this.laroux);
-;(function(laroux) {
-    "use strict";
-
     // cookies
     laroux.cookies = {
         get: function(name) {
