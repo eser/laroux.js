c25776194187f642c76872b2879320926fbad39f
Eser Ozvataf <eser@sent.com>
2014-11-16 00:09:50 +0200

    * added defaultValue for $l.cookies.get. * added expires for $l.cookies.set.

--- src/laroux.cookies.js
+++ src/laroux.cookies.js
@@ -3,17 +3,31 @@
 
     // cookies
     laroux.cookies = {
-        get: function(name) {
+        get: function(name, defaultValue) {
+            if (typeof defaultValue == 'undefined') {
+                defaultValue = null;
+            }
+
             var re = new RegExp(name + '=[^;]+', 'i');
             if (!document.cookie.match(re)) {
-                return null;
+                return defaultValue;
             }
 
             return document.cookie.match(re)[0].split('=')[1];
         },
 
-        set: function(name, value) {
-            document.cookie = name + '=' + value + '; path=' + laroux.baseLocation;
+        set: function(name, value, expires) {
+            var expireValue = '';
+            if (typeof expires != 'undefined' || expires !== null) {
+                expireValue = '; expires=' + expires.toGMTString();
+            }
+
+            var pathValue = laroux.baseLocation;
+            if (pathValue.length === 0) {
+                pathValue = '/';
+            }
+
+            document.cookie = name + '=' + value + expireValue + '; path=' + pathValue;
         }
     };
 
