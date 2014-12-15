686be7adb3c53c5f9432c6394c3841a12f03823d
larukedi <eser@sent.com>
2014-12-15 10:51:32 +0200

    * lighter, more compatible mvc.

--- src/laroux.mvc.js
+++ src/laroux.mvc.js
@@ -7,12 +7,15 @@
     // mvc
     laroux.mvc = {
         apps: {},
+        pauseUpdate: false,
 
         init: function(element, model) {
             var appKey = element.getAttribute('id');
 
             model.onupdate = function(event) {
-                laroux.mvc.update(appKey, [event.key]);
+                if (!laroux.mvc.pauseUpdate) {
+                    laroux.mvc.update(appKey, [event.key]);
+                }
             };
 
             laroux.mvc.apps[appKey] = {
@@ -29,12 +32,43 @@
         rebind: function(appKey) {
             var app = laroux.mvc.apps[appKey];
             app.modelKeys = laroux.helpers.getKeysRecursive(app.model._data); // FIXME: works only for $l.stack
-            app.boundElements = [];
+            app.boundElements = {};
             app.eventElements = [];
 
             laroux.mvc.scanElements(app, app.element);
             laroux.mvc.update(appKey);
-            // TODO: bind events for eventElements
+
+            var fnc = function(ev, elem) {
+                var setFields = elem.getAttribute('lr-set');
+                if (setFields === null) {
+                    return;
+                }
+
+                var binding = laroux.mvc.bindStringParser(setFields)
+                // laroux.mvc.pauseUpdate = true;
+                for (var item in binding) {
+                    if (!binding.hasOwnProperty(item)) {
+                        continue;
+                    }
+
+                    if (binding[item].charAt(0) == '\'') {
+                        app.model[item] = binding[item].substring(1, binding[item].length - 1);
+                    } else if (binding[item].substring(0, 5) == 'attr.') {
+                        app.model[item] = elem.getAttribute(binding[item].substring(5));
+                    } else if (binding[item].substring(0, 5) == 'prop.') {
+                        app.model[item] = elem[binding[item].substring(5)];
+                    }
+                }
+                // laroux.mvc.pauseUpdate = false;
+            };
+
+            for (var i = 0, length = app.eventElements.length; i < length; i++) {
+                laroux.dom.setEvent(
+                    app.eventElements[i].element,
+                    app.eventElements[i].event,
+                    fnc
+                );
+            }
         },
 
         scanElements: function(app, element) {
@@ -86,206 +120,19 @@
                 var boundElement = app.boundElements[keys[i]];
 
                 for (var j = 0, length2 = boundElement.length; j < length2; j++) {
-                    switch (boundElement[j].target) {
-                        case 'content':
-                            boundElement[j].element.textContent = laroux.helpers.getElement(app.model, keys[i]);
-                            break;
+                    if (boundElement[j].target == 'content') {
+                        boundElement[j].element.textContent = laroux.helpers.getElement(app.model, keys[i]);
+                    } else if (boundElement[j].target.substring(0, 6) == 'style.') {
+                        boundElement[j].element.style[boundElement[j].target.substring(6)] = laroux.helpers.getElement(app.model, keys[i]);
+                    } else if (boundElement[j].target.substring(0, 5) == 'attr.') {
+                        // FIXME removeAttribute on null value?
+                        boundElement[j].element.setAttribute(boundElement[j].target.substring(5), laroux.helpers.getElement(app.model, keys[i]));
+                    } else if (boundElement[j].target.substring(0, 5) == 'prop.') {
+                        // FIXME removeAttribute on null value?
+                        boundElement[j].element[boundElement[j].target.substring(5)] = laroux.helpers.getElement(app.model, keys[i]);
                     }
                 }
             }
-
-/*
-            if (app.cachedNodes === null) {
-                for (var i1 in app.boundElements) {
-                    if (!app.boundElements.hasOwnProperty(i1)) {
-                        continue;
-                    }
-
-                    var item1 = app.boundElements[i1];
-
-                    laroux.dom.unsetEvent(
-                        item1.element,
-                        laroux.mvc.getRelatedEventName(item1.element),
-                        laroux.mvc.onBoundFieldChange
-                    );
-                    item1.element.removeAttribute('lr-app-id');
-                    item1.element.removeAttribute('lr-bind-id');
-                }
-
-                app.cachedNodes = [];
-                app.boundElements = [];
-                laroux.mvc.scanElements(app, app.element);
-
-                for (var i2 in app.boundElements) {
-                    if (!app.boundElements.hasOwnProperty(i2)) {
-                        continue;
-                    }
-
-                    var item2 = app.boundElements[i2];
-                    var value = laroux.mvc.getRelatedValue(item2.element, true);
-                    if (value !== null) {
-                        app.model[item2.key] = value;
-                    }
-
-                    laroux.dom.setEvent(
-                        item2.element,
-                        laroux.mvc.getRelatedEventName(item2.element),
-                        laroux.mvc.onBoundFieldChange
-                    );
-                    item2.element.setAttribute('lr-app-id', appKey);
-                    item2.element.setAttribute('lr-bind-id', i2);
-                }
-            }
-
-            for (var i3 in app.cachedNodes) {
-                if (!app.cachedNodes.hasOwnProperty(i3)) {
-                    continue;
-                }
-
-                var item3 = app.cachedNodes[i3];
-
-                if (keys !== undefined && laroux.aindex(keys, item3.key) === -1) {
-                    continue;
-                }
-
-                if (item3.node instanceof Attr) {
-                    item3.node.value = item3.value;
-                } else {
-                    item3.node.textContent = item3.value;
-                }
-            }
-
-            for (var i4 in app.cachedNodes) {
-                if (!app.cachedNodes.hasOwnProperty(i4)) {
-                    continue;
-                }
-
-                var item4 = app.cachedNodes[i4];
-
-                if (keys !== undefined && laroux.aindex(keys, item4.key) === -1) {
-                    continue;
-                }
-
-                var findStr = '{{' + item4.key + '}}';
-                var objectValue = laroux.helpers.getElement(app.model, item4.key);
-
-                if (item4.node instanceof Attr) {
-                    item4.node.value = item4.node.value.replace(findStr, objectValue);
-                } else {
-                    item4.node.textContent = item4.node.textContent.replace(findStr, objectValue);
-                }
-            }
-
-            for (var i5 in app.boundElements) {
-                if (!app.boundElements.hasOwnProperty(i5)) {
-                    continue;
-                }
-
-                var item5 = app.boundElements[i5];
-
-                if (keys !== undefined && laroux.aindex(keys, item5.key) === -1) {
-                    continue;
-                }
-
-                laroux.mvc.setRelatedValue(item5.element, app.model[item5.key]);
-            }
-
-            // Object.observe(app.model, laroux.mvc.observer);
-*/
-        },
-
-        getRelatedEventName: function(element) {
-            switch (element.tagName) {
-                case 'INPUT':
-                    switch (element.getAttribute('type').toUpperCase()) {
-                        case 'BUTTON':
-                        case 'SUBMIT':
-                        case 'RESET':
-                            return 'click';
-                    }
-
-                    // return 'change';
-                    return 'keyup';
-
-                case 'TEXTAREA':
-                    // return 'change';
-                    return 'keyup';
-
-                case 'BUTTON':
-                    return 'click';
-            }
-
-            return null;
-        },
-
-        getRelatedValue: function(element, initial) {
-            switch (element.tagName) {
-                case 'INPUT':
-                    switch (element.getAttribute('type').toUpperCase()) {
-                        case 'BUTTON':
-                        case 'SUBMIT':
-                        case 'RESET':
-                            if (initial === true) {
-                                return null;
-                            }
-
-                            return element.getAttribute('lr-value');
-
-                        case 'CHECKBOX':
-                        case 'RADIO':
-                            return element.checked;
-                    }
-
-                    return element.value;
-
-                case 'TEXTAREA':
-                    return element.value;
-
-                case 'BUTTON':
-                    if (initial === true) {
-                        return null;
-                    }
-
-                    return element.getAttribute('lr-value');
-            }
-
-            return null;
-        },
-
-        setRelatedValue: function(element, value) {
-            switch (element.tagName) {
-                case 'INPUT':
-                    switch (element.getAttribute('type').toUpperCase()) {
-                        case 'BUTTON':
-                        case 'SUBMIT':
-                        case 'RESET':
-                            return;
-
-                        case 'CHECKBOX':
-                        case 'RADIO':
-                            element.value = !!value;
-                            return;
-                    }
-
-                    element.value = value;
-                    return;
-
-                case 'TEXTAREA':
-                    element.value = value;
-                    return;
-            }
-        },
-
-        onBoundFieldChange: function(event, element) {
-            var appId = element.getAttribute('lr-app-id');
-            var bindId = element.getAttribute('lr-bind-id');
-
-            var selectedApp = laroux.mvc.apps[appId];
-            var selectedBind = selectedApp.boundElements[bindId];
-
-            // Object.unobserve(selectedApp.model, laroux.mvc.observer);
-            selectedApp.model[selectedBind.key] = laroux.mvc.getRelatedValue(element);
-            // Object.observe(selectedApp.model, laroux.mvc.observer);
         },
 
         bindStringParser: function(text) {
